924a274a549876a99f2c9f1214679d76b5440949
unknown <476400902@qq.com>
2015-03-22 10:20:53 +0800

    2015.3.22

---
+++ dantefung/src/com/dantefung/ClassLoader/ClassLoaderAttachment.java
@@ -0,0 +1,11 @@
+package com.dantefung.ClassLoader;
+
+import java.util.Date;
+//复制该类的完整路径名称："I:\百度云同步盘\dantefung\bin\com\dantefung\classloader\ClassLoaderAttachment.class"
+//Run as -->Open Run Dialog..-->
+public class ClassLoaderAttachment extends Date {
+	public String toString()
+	{
+		return "dantefung";
+	}
+}
---
+++ dantefung/src/com/dantefung/ClassLoader/MyClassLoaderTest.java
@@ -0,0 +1,69 @@
+package com.dantefung.ClassLoader;
+
+import java.io.ByteArrayOutputStream;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.Date;
+//模板设计模式。
+//Run as -->Open Run Dialog..（Run configuration）-->
+public class MyClassLoaderTest extends ClassLoader{
+
+	public static void main(String[] args) throws Exception {
+		// TODO Auto-generated method stub
+		String srcPath = args[0];//I:\百度云同步盘\dantefung\bin\com\dantefung\classloader\ClassLoaderAttachment.class
+		String destDir = args[1];//dantelib
+		FileInputStream fis = new FileInputStream(srcPath);
+		String destFileName = srcPath.substring(srcPath.lastIndexOf('\\')+1);
+		String destPath = destDir + "\\" +destFileName;
+		FileOutputStream fos =new FileOutputStream(destPath);
+		cypher(fis,fos);
+		fos.close();
+		fis.close();
+		
+		//System.out.println(new ClassLoaderAttachment().toString());
+	   Class clazz = new MyClassLoaderTest("dantelib").loadClass("ClassLoaderAttachment");
+	   Date dl = (Date)clazz.newInstance();
+	   System.out.println(dl);
+	}
+
+	private static void cypher(InputStream ips, OutputStream ops) throws Exception
+	{
+		int b = -1;
+		while((b=ips.read()) != -1)
+		{
+			ops.write(b*0xff);
+		}
+	}
+
+	private String classDir;
+		
+	@Override
+	protected Class<?> findClass(String name) throws ClassNotFoundException {
+		String classFileName = classDir + "\\" + name + ".class";//得到该类的路径
+		FileInputStream fis;
+		try {
+			fis = new FileInputStream(classFileName);//用FileInputStrem加载	
+			ByteArrayOutputStream bos = new ByteArrayOutputStream();
+			cypher(fis,bos);
+			fis.close();
+			byte[] bytes = bos.toByteArray();
+		return  defineClass(bytes,0,bytes.length);//转成字节码。
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return super.findClass(name);
+	}
+	
+	public MyClassLoaderTest()
+	{
+		
+	}
+	
+	public MyClassLoaderTest(String classDir)
+	{
+	    this.classDir = classDir;
+	}
+}
