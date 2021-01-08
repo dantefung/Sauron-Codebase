8787ad96aad5b44083d37e129de592e0a33dce97
fenghaolin <476400902@qq.com>
2021-01-08 12:04:10 +0800

    注解 & 动态代理代码更新

---
+++ solon-kernel-codebase/src/main/java/com/dantefung/concurrent/basic/VolatileTest.java
@@ -0,0 +1,25 @@
+package com.dantefung.concurrent.basic;
+
+public class VolatileTest {
+
+    private volatile int volatileCount = 0;
+    private int count = 0;
+
+    public static void main(String[] args) {
+
+        VolatileTest volatileTest = new VolatileTest();
+
+        volatileTest.increase();
+        volatileTest.decrease();
+
+    }
+
+    private void decrease() {
+        count--;
+    }
+
+    private void increase() {
+        volatileCount++;
+    }
+
+}
---
+++ solon-kernel-codebase/src/main/java/com/dantefung/dp/proxy/sample01/Subject.java
@@ -0,0 +1,6 @@
+package com.dantefung.dp.proxy.sample01;
+
+//目标接口，对应ITraget
+public interface Subject {
+    void hello(String str);
+}
