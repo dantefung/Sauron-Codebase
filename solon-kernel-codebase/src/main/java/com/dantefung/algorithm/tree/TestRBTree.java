2ce06d749e498e8717a4456888a2571d5af6c686
fenghaolin <476400902@qq.com>
2021-09-30 16:12:45 +0800

    红黑树

---
+++ solon-kernel-codebase/src/main/java/com/dantefung/algorithm/tree/TestRBTree.java
@@ -0,0 +1,18 @@
+package com.dantefung.algorithm.tree;
+
+import java.util.Scanner;
+
+public class TestRBTree {
+    public static void main(String[] args) {
+        RBTree<String, Object> rbt = new RBTree();
+        //测试输入：ijkgefhdabc
+        while(true) {
+            Scanner sc = new Scanner(System.in);
+            System.out.println("请输入key:");
+            String key = sc.next();
+
+            rbt.insert(key, null);
+            TreeOperation.show(rbt.getRoot());
+        }
+    }
+}
