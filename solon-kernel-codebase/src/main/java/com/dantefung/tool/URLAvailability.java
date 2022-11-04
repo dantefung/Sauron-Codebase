77a4fb6926cbd408aaa83a78d1fa759ad9b59133
fenghaolin <476400902@qq.com>
2022-11-04 15:17:39 +0800

    update

--- solon-kernel-codebase/src/main/java/com/dantefung/tool/URLAvailability.java
+++ solon-kernel-codebase/src/main/java/com/dantefung/tool/URLAvailability.java
@@ -1,8 +1,12 @@
 package com.dantefung.tool;
 
+import org.springframework.util.StringUtils;
+
+import java.io.IOException;
 import java.net.HttpURLConnection;
 
 import java.net.URL;
+import java.net.URLConnection;
 
 /**
  * 文件名称为：URLAvailability.java
@@ -78,13 +82,76 @@ public class URLAvailability {
 		return url;
 	}
 
+	/**
+	 * 判断链接是否有效
+	 * 输入链接
+	 * 返回true或者false
+	 */
+	public static boolean isValid(String strLink) {
+		URL url;
+		try {
+			url = new URL(strLink);
+			HttpURLConnection connt = (HttpURLConnection)url.openConnection();
+			connt.setRequestMethod("HEAD");
+			String strMessage = connt.getResponseMessage();
+			if (strMessage.compareTo("Not Found") == 0) {
+				return false;
+			}
+			connt.disconnect();
+		} catch (Exception e) {
+			return false;
+		}
+		return true;
+	}
 
-	public static void main(String[] args) {
+
+	public static void main(String[] args) throws IOException {
 
 		URLAvailability u = new URLAvailability();
 
 		u.isConnect("http://www.baidu.com");
 
+		try {
+			URL url=new URL( "http://www.dukai168.cn");
+			URLConnection conn=url.openConnection();
+			String str=conn.getHeaderField(0);
+			if (str.indexOf( "OK ")> 0)
+			{
+				System.out.println( "正常! ");
+			}else{
+				System.out.println( "不能游览 ");
+			}
+		} catch (Exception ex) {
+		}
+
+		System.out.println(isValid("http://localhost:8090/Demo/clean.sql"));
+		// 方法一
+		test1();
+		// 方法二：
+		test2();
+
+	}
+
+	private static void test2() throws IOException {
+		URL url = new URL("http://localhost:8090/Demo/clean.sql");
+		HttpURLConnection urlcon2 = (HttpURLConnection) url.openConnection();
+		Long TotalSize=Long.parseLong(urlcon2.getHeaderField("Content-Length"));
+		if (TotalSize>0){
+			System.out.println("存在");
+		}else{
+			System.out.println("不存在");
+		}
+	}
+
+	private static void test1() throws IOException {
+		URL serverUrl = new URL("http://localhost:8090/Demo/clean.sql");
+		HttpURLConnection urlcon = (HttpURLConnection) serverUrl.openConnection();
+		String message = urlcon.getHeaderField(0);
+		if (StringUtils.hasText(message) && message.startsWith("HTTP/1.1 404")) {
+			System.out.println("不存在");
+		}else{
+			System.out.println("存在"+message);
+		}
 	}
 
 }
